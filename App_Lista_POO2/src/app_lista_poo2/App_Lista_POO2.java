58315c76cee81f5303fcd279d948318565458a83
GhostCodeCo <113939428+GhostCodeCo@users.noreply.github.com>
2023-10-24 22:49:02 -0500

    Add files via upload

---
+++ App_Lista_POO2/src/app_lista_poo2/App_Lista_POO2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app_lista_poo2;
+
+/**
+ *
+ * @author AP-SE105-01
+ */
+public class App_Lista_POO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
